a7b9ed38160954db0e5b9f182e8b3c7c2613ee99
Eric Pan <ericpan0207@gmail.com>
2019-10-25 18:28:46 -0700

    Remove duplicate files

--- main.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-  //cycle_through_colors();
-  //run_traffic();
-  //cycle_through_colors_using_interrupt();
-  //blink_blue_interrupt();
-  //run_traffic_interrupt();
-  return 0;
-}
